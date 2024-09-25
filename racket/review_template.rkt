64fd2afa9b04b40fbab5e305c83668ddb84e721b
JonAlf <jonalf@gmail.com>
2024-09-25 14:06:10 -0400

    +08

---
+++ racket/review_template.rkt
@@ -0,0 +1,36 @@
+#|
+MC Question:
+What is the air speed of an unladen swallow?
+Options:
+A) 2 mph
+B) 20 mph
+C) 200 mph
+D) African or European?
+
+Answer: D
+|#
+
+#|
+MC Question:
+Who you gonna call?
+Options:
+A) Transformers
+B) Ghostbusters
+C) Thundercats
+D) Teenage Mutant Ninja Turtles
+
+Answer: B
+|#
+
+#|
+Programming question:
+Write a function that takes the radius of a
+circle and returns it's area.
+area = pi * r^2
+
+Possible solution:
+|#
+(define areaC
+  (lambda (radius)
+    (* pi radius radius)))
+(areaC 10)
---
+++ racket/work08.rkt
@@ -0,0 +1,90 @@
+#lang racket
+
+;(isEven n) --> boolean
+;  n : integer
+;
+; Returns true or false based on if n is even or not
+; (isEven 45) --> #f
+; (isEvent 8) --> #t
+(define isEven
+  (lambda (n)
+    (= 0 (remainder n 2))))
+(display "isEven tests:\n")
+(display "#f: ")
+(isEven 45)
+(display "#t: ")
+(isEven 8)
+
+;(isMultiple n m) --> boolean
+;  n --> integer
+;  m --> integer
+;
+;  Returns true if n is a multiple of m
+;  false otherwise
+;  (isMultiple 21 7) --> #t
+;  (isMultiple 34 4) --> #f
+(define isMultiple
+  (lambda (n m)
+    (= 0 (remainder n m))))
+(display "isMultiple tests:\n")
+(display "#t: ")
+(isMultiple 21 7)
+(display "#f: ")
+(isMultiple 34 4)
+
+;(twoDigits n) --> boolean
+;  n --> integer
+;
+;  Returns true if n is a two digit integer,
+;  false otherwise
+;  (twoDigits 51) --> #t
+;  (twoDigits 2) --> #f
+;  (twoDigits 1234) --> #f
+(define twoDigits
+  (lambda (n)
+    (and
+     (= 0 (quotient n 100))
+     (< 0 (quotient n 10)))))
+(display "twoDigits tests:\n")
+(display "#t: ")
+(twoDigits 51)
+(display "#f: ")
+(twoDigits 2)
+(display "#f: ")
+(twoDigits 1234)
+
+;(twoDigits n x) --> boolean
+;  n --> integer
+;  x --> integer
+;  Returns true if n is an x digit integer,
+;  false otherwise
+;  (xDigits 32 1) --> #f
+;  (xDigits 3 1) --> #t
+;  (xDigits 1234 4) --> #t
+(define xDigits
+  (lambda (n x)
+    (and
+     (= 0 (quotient n (expt 10 x)))
+     (< 0 (quotient n (expt 10 (- x 1)))))))
+(display "xDigits tests:\n")
+(display "#f: ")
+(xDigits 32 1)
+(display "#t: ")
+(xDigits 3 1)
+(display "#t: ")
+(xDigits 1234 4)
+
+
+;Alternate, arguably simpler, versions
+;of twoDigits and xDigits
+(define twoDigits2
+  (lambda (n)
+    (and
+     (< n 100))
+     (>= n 10)))
+
+(define xDigits2
+  (lambda (n x)
+    (and
+     (< n (expt 10 x))
+     (>= n (expt 10 (- x 1))))))
