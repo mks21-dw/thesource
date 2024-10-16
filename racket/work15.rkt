a40586f5f5c507747f7d1b3050b2f2dfa5beaa4d
JonAlf Dyrland-Weaver <dw@stuy.edu>
2024-10-16 14:49:53 -0400

    more comments in work15

--- racket/work15.rkt
+++ racket/work15.rkt
@@ -3,29 +3,45 @@
   (lambda (n maxValue)
     (build-list n (lambda (x) (random maxValue)))))
 
+;randlist test
 (display "list: ")
 (define g (randList 20 100)) 
 g
 
+;sum test
 (display "sum: ")
-(foldl + 0 g)
+(foldl +  ;function
+       0  ;starting valu
+       g) ;list
+
+;count test
 (display "count: ")
-(foldl (lambda (value previous)
-         (+ 1 previous)) 0 g)
+(foldl (lambda (value previous) ;function
+         (+ 1 previous))
+       0 ;starting value
+       g) ;list
+
+;find smallest
 (display "smallest ")
-(foldl (lambda (value previous)
+(foldl (lambda (value previous)  ;function
          (if (< value previous)
              value
              previous))
-       (first g) g)
+       (first g) ;starting value
+       g) ;list
 
+;filter odds
 (display "odds: ")
 (define odds (filter odd? g))
 odds
-(display "sum: ")
+
+;sum of odds
+(display "oods sum: ")
 (foldl + 0 odds)
+
+;larger half
 (display "larger half count: ") 
-(foldl (lambda (value previous)
+(foldl (lambda (value previous) ;function
          (+ 1 previous))
-       0
-       (filter (lambda (x) (>= x 25)) g))
+       0 ;starting value
+       (filter (lambda (x) (>= x 50)) g)) ;list
